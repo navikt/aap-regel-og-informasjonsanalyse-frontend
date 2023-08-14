f2cf99a3ff9457fc65dcf064019f273b49c1e8c4
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-08-14 10:25:04 +0200

    :construction: WIP

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+    basePath: '/aap/regelverk',
+    trailingSlash: true,
+    reactStrictMode: true,
+    output: 'standalone',
+    assetPrefix: process.env.ASSET_PREFIX ?? undefined,
+  
+    experimental: {
+      instrumentationHook: true,
+    },
+  
+    i18n: {
+      locales: ['nb'],
+      defaultLocale: 'nb',
+    },
+  };
+  
+  module.exports = nextConfig;
